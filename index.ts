dfbb24bc5749468e1b63b40a19ce7910e674e7d4
nate <natephiliphoffman@proton.me>
2026-01-16 15:28:21 -0500

    fixing events from double registering

--- index.ts
+++ index.ts
@@ -8,24 +8,46 @@ class Root extends Component<never, never> {
 
     render() {
         return `<div>
-            <h1>Welcome to my component2!</h1>
+            <h1>Welcome to this example!</h1>
             <c-clicker></c-clicker>
         </div>`;
     }
 }
 
-class Clicker extends Component<{ clicked: string }, { input?: string | null }> {
+type ClickerState = { clicked: string }
+type ClickerProfile = { input?: string | null, input2?: string | null }
+
+class Clicker extends Component<ClickerState, ClickerProfile> {
     constructor() {
-        super({ clicked: "default click" }, { input: "default" });
+        super({ clicked: "has not been clicked" }, { input: "this text saves to profile", input2: "calls safeRefresh onchange" });
     }
 
     render() {
-        this.onClick('p.hello', () => this.setState({ clicked: "clicked!" }));
-        this.onChange('input', () => this.profile.input = this.querySelector('input')?.value);
+        this.onClick('p', () => this.setState({ clicked: "was clicked!" }));
+        this.onChange('input.one', (e) => this.profile.input = (e.target as HTMLInputElement).value);
+        
+        this.onChange('input.two', (e) => {
+            this.profile.input2 = (e.target as HTMLInputElement).value;
+            this.safeRefresh(1500);
+        });
 
         return /* HTML */`
-            <p>This is a child <p class="hello">hello ${this.state.clicked}</p>.</p>
-            <input type="text" class="input-box" value="${this.profile.input}"/>
+            <p>This text has an onclick handler on it and "${this.state.clicked}" this is tracked with state.</p>
+            <br/>
+            <div>Below is an input field that onchange writes its value to the profile but not to state</div>
+            <input type="text" class="one" value="${this.profile.input}"/>
+            <div>Since profile is not written to state a refresh is not normally triggered unless one is triggered elsewhere.</div>
+            <div>The value currently in the profile for this field is "${this.profile.input}".  You can click the first text above to trigger a rerender to update this text.</div>
+            <br/>
+
+            <div>A solution to rerendering data with something not in state is to use safeRefresh(), calling refresh() is highly illadvised </div>
+            <div>Saferefresh debounces itself, so that multiple calls to it will not trigger it often, this time can be bypassed</div>
+            <br/>
+            
+            <div>Below is an input field that triggers the safeRefresh function onchange, it is set to 2 seconds which means you must leave it alone for 2 seconds to update the text below it</div>
+            <input type="text" class="two" value="${this.profile.input2}"/>
+            <div>Here is the text currently stored: ${this.profile.input2}</div>
+
         `
     }
 }
--- src/component.ts
+++ src/component.ts
@@ -1,10 +1,10 @@
 
 type Trigger = string;
-type Handler = ()=>void;
+type Handler = (e: Event) => void;
 type Selector = string;
 type CustomElementName = string;
 
-export type Event = {
+export type ComponentEvent = {
     trigger: Trigger
     handler: Handler
     selector: Selector
@@ -29,7 +29,8 @@ export function defineElements(arr: [CustomElementName, ComponentConstructor][])
 export class Component<T extends State, P extends Profile> extends HTMLElement {
 
     state: T;
-    events: Event[] = [];
+    events: ComponentEvent[] = [];
+    safeRefreshId: number = 0;
 
     // this is used to store user preferences without updating state such as storing input field data
     // this is useful when wanting to store data you can easily look at later for retrieval rather than dom fetching
@@ -43,6 +44,7 @@ export class Component<T extends State, P extends Profile> extends HTMLElement {
 
     // Base Methods:
     connectedCallback() {
+        
         this.refresh();
     }
 
@@ -52,44 +54,73 @@ export class Component<T extends State, P extends Profile> extends HTMLElement {
     }
 
     setState(state: T) {
+        console.log("was clicked");
         this.state = state;
         this.refresh();
     }
 
+    // can be used to trigger a manual refresh, this is considered much more performant and safer 
+    // than calling redirect directly
+    safeRefresh(ms?: number) {
+        clearTimeout(this.safeRefreshId);
+        this.safeRefreshId = setTimeout(this.refresh.bind(this), ms || 500);
+    }
+
     // Added Methods:
     refresh() {
-        this.innerHTML = this.render();
+        //console.log("number of events is ", this.events.length)
+        //console.log(this.events);
+        console.log("refresh called");
         this.removeEvents();
+        this.innerHTML = this.render();
         this.registerEvents();
+        console.log(this.events);
     }
 
     onClick(selector: Selector, handler: Handler) {
+        console.log("onclick called");
         this.onEvent("click", selector, handler)
     }
 
     onChange(selector: Selector, handler: Handler) {
         this.onEvent("change", selector, handler)
     }
-
+    /*
+        onInput(selector: Selector, handler: Handler, delay: number = 250) {
+    
+            const debouncedHandler = handler;
+    
+            this.onChange(selector, );
+            this.keyup
+            
+        }
+    
+        onKeyup(selector: Selector, handler: Handler) {
+    
+        }
+    */
     onEvent(trigger: Trigger, selector: Selector, handler: Handler) {
         this.events.push({ selector, trigger, handler });
     }
 
+    // must register the events after they are added due to the composition of events happening before the return of the html
     registerEvents() {
         this.events.forEach(({ selector, trigger, handler }) => {
             const elements = this.querySelectorAll(selector);
             elements.forEach((element) => {
                 element.addEventListener(trigger, handler);
-                this.events.push({ selector, trigger, handler });
             });
         });
     }
+        
 
     removeEvents() {
         this.events.forEach(({ selector, trigger, handler }) => {
             const elements = this.querySelectorAll(selector);
             elements.forEach((el) => el.removeEventListener(trigger, handler));
         });
+
+        this.events = [];
     }
 
     render(): string {
